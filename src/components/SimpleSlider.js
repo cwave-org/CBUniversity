2c589b669762db772a730419d0170137a102d7a4
Nayeon Ryu <2014027@sookmyung.ac.kr>
2022-12-26 20:54:23 +0900

    [fix] 댓글 디자인 수정

--- src/components/SimpleSlider.js
+++ src/components/SimpleSlider.js
@@ -1,47 +1,53 @@
 // Slider는 해당 컴포넌트에서 import해주자
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import { StyledSlider } from './StyledSlider';
-import { useEffect, useState } from 'react';
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { StyledSlider } from "./StyledSlider";
+import { useEffect, useState } from "react";
 
 const SimpleSlick = () => {
-  const [imgs,setImgs]=useState([]);
+  const [imgs, setImgs] = useState([]);
   const sliderSettings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    arrow:false,
+    arrow: false,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
   };
-  useEffect(()=>{
+  useEffect(() => {
     setImgs([
-        {
-          id:1,
-          src:"img/carr1.png",
-          url:"https://flyup-startup.com/sub/invest_status.php"
-        },
-        {
-          id:2,
-          src:"img/carr2.png",
-          url:"https://instagram.com/cwave_?igshid=YmMyMTA2M2Y="
-        }]);
-  },[]);
-  const onClick=(url)=>{
+      {
+        id: 1,
+        src: "img/carr1.png",
+        url: "https://flyup-startup.com/sub/invest_status.php",
+      },
+      {
+        id: 2,
+        src: "img/carr2.png",
+        url: "https://instagram.com/cwave_?igshid=YmMyMTA2M2Y=",
+      },
+    ]);
+  }, []);
+  const onClick = (url) => {
     console.log(url);
-    window.open(url,"_blanck");
-  }
+    window.open(url, "_blanck");
+  };
   return (
     // <Container>
-      <StyledSlider {...sliderSettings}>
-        {imgs&&imgs.map((key)=>(
-            <img onClick={()=>onClick(key.url)} key={key.id} src={key.src} alt="배너"/>
+    <StyledSlider {...sliderSettings}>
+      {imgs &&
+        imgs.map((key) => (
+          <img
+            onClick={() => onClick(key.url)}
+            key={key.id}
+            src={key.src}
+            alt="배너"
+          />
         ))}
-      </StyledSlider>
+    </StyledSlider>
     // </Container>
   );
 };
